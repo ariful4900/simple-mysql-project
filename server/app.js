c96b8faa7ebaeacd21cf3fc20f9089b80d833969
nodeAriful <ariful.node82@gmail.com>
2021-02-03 23:32:38 +0600

    create basic setup this project

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config();
+
+app.use(cors())
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+
+//create
+app.post('/insert', (req, res) => {
+    
+})
+
+
+//read
+app.get('/getAll', (req, res)=>{
+    console.log('text');
+})
+
+
+// update
+
+
+
+
+// delete
