8a34547f88b7dd3b2dd9e9c19d46658e0db4027e
nodeAriful <ariful.node82@gmail.com>
2021-02-04 00:12:21 +0600

    connect server and work done

--- client/index.js
+++ client/index.js
@@ -1,17 +1,26 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    
-    
+// const { json } = require("body-parser");
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('http://localhost:5000/getAll')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(e => {
+            console.log(e.message);
+        })
+
     loadHTMLTable([])
 });
 
-const loadHTMLTable= (data)=>{
+const loadHTMLTable = (data) => {
 
     const table = document.querySelector('table tbody');
 
     // let tableHTML = '';
-    if(data.length === 0){
+    if (data.length === 0) {
         table.innerHTML = '<tr><td class="no-data" colspan="5"> No Data</td></tr>'
-    }else{
+    } else {
         table.innerHTML = '<tr></tr>'
     }
 }
--- server/app.js
+++ server/app.js
@@ -1,30 +1,24 @@
 const express = require('express');
+require('dotenv').config();
+// const bodyPerser = require('body-parser')
+
 const app = express();
-const cors = require('cors')
-const dotenv = require('dotenv')
-dotenv.config();
 
-app.use(cors())
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({extended: false}));
 
 
-//create
-app.post('/insert', (req, res) => {
-    
+app.get('/getAll', (req, res) => {
+    res.json({
+        success: true
+    })
+});
+
+const port = process.env.PORT; 
+
+app.listen(port, ()=>{
+    console.log(` Application is running on port ${port}`);
 })
-
-
-//read
-app.get('/getAll', (req, res)=>{
-    console.log('text');
-})
-
-
-// update
-
-
-
-
-// delete
